9e058d2ab6c2e5a167432e0e9e38cc3dd3585d30
Tom Pinargote <tpinargote@fi.uba.ar>
2026-08-22 18:10:06 -0300

    implementing short read/write tolerant socket ops for sever

--- services/server/src/safe_socket/safe_socket.py
+++ services/server/src/safe_socket/safe_socket.py
@@ -1,11 +1,22 @@
 import socket
 
-# TODO: Complete with a short-read/short-write tolerant implementation
+
+def recv_all(sock: socket.socket, size: int) -> bytes:
+    chunks = []
+    bytes_recd = 0
+    while bytes_recd < size:
+        chunk = sock.recv(size - bytes_recd)
+        if chunk == b"":
+            raise ConnectionError("socket connection broken")
+        chunks.append(chunk)
+        bytes_recd += len(chunk)
+    return b"".join(chunks)
 
 
-def recv_all(socket: socket.socket, size):
-    return socket.recv(size)
-
-
-def send_all(socket: socket.socket, bytes):
-    return socket.send(bytes)
+def send_all(sock: socket.socket, data: bytes):
+    total_sent = 0
+    while total_sent < len(data):
+        sent = sock.send(data[total_sent:])
+        if sent == 0:
+            raise ConnectionError("socket connection broken")
+        total_sent += sent
