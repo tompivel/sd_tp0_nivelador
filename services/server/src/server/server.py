14b10482164d408da8c8dc9f00c1f3304e0bd67e
Tom Pinargote <tpinargote@fi.uba.ar>
2026-09-09 12:52:34 -0300

    encapsulating Rwlock implementation into a class

--- services/server/src/server/server.py
+++ services/server/src/server/server.py
@@ -1,9 +1,9 @@
-import contextlib
 import signal
 import socket
 import threading
 
 import logger
+import utils.rwlock
 from lottery.lottery import Lottery
 
 from . import protocol
@@ -25,42 +25,13 @@ class Server:
         self.server_port = server_port
         self.lottery = Lottery(storage_path)
         self.agency_quorum_min = agency_quorum_min
-        self.rw_condition = threading.Condition()
-        self.readers = 0
-        self.writers = 0
+        self.rwlock = utils.rwlock.RWLock()
         self.socket_lock = threading.Lock()
         self.draw_barrier = threading.Barrier(self.agency_quorum_min)
         self.active_sockets = []
 
         # Register the signal handler
         signal.signal(signal.SIGTERM, self.handle_sigterm)
-
-    @contextlib.contextmanager
-    def _read_lock(self):
-        with self.rw_condition:
-            while self.writers > 0:
-                self.rw_condition.wait()
-            self.readers += 1
-        try:
-            yield
-        finally:
-            with self.rw_condition:
-                self.readers -= 1
-                if self.readers == 0:
-                    self.rw_condition.notify_all()
-
-    @contextlib.contextmanager
-    def _write_lock(self):
-        with self.rw_condition:
-            while self.writers > 0 or self.readers > 0:
-                self.rw_condition.wait()
-            self.writers += 1
-        try:
-            yield
-        finally:
-            with self.rw_condition:
-                self.writers -= 1
-                self.rw_condition.notify_all()
 
     def handle_sigterm(self, signum, frame):
         raise GracefulExit()
@@ -83,7 +54,7 @@ class Server:
                     if bets:
                         agency_id = bets[0].agency_id
 
-                    with self._write_lock():
+                    with self.rwlock.write_lock():
                         self.lottery.store_bets(bets)
 
                     protocol.send_message(client_socket, protocol.OP_BATCH_ACK, b"")
@@ -96,7 +67,7 @@ class Server:
 
                     winners = []
                     # Find winners specifically for this agency
-                    with self._read_lock():
+                    with self.rwlock.read_lock():
                         for bet in self.lottery.load_bets():
                             if bet.agency_id == agency_id and self.lottery.has_won(bet):
                                 winners.append(bet)
